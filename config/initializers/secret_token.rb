3d9a9bd00d44ff87857aba24a84c494ec51a1406
Chris Doornink <cdoornink@rhapsody.com>
2011-05-31 08:26:05 -0700

    Rails structure added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songfeedback::Application.config.secret_token = '03aab137a716388003f741998b1049896963014652f6a3e4d9c2b4e579e5cbb48ab9da1974354400d310219fddfc85788c0b644d71bced5111fda32e22c94419'
