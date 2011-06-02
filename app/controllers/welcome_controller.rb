98e85dd2d590b9901cd3dcddb80b375b320aecab
Chris Doornink <cdoornink@rhapsody.com>
2011-06-02 13:16:38 -0700

    general layout design complete (rough)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @title = "Songs will play here."
+    @artist = "Choose a song on the right to get started."
+    @desc = ""
+    @username = "chrisdoornink@gmail.com"
   end
 
   def show
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,52 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $("#menu").delegate("#menu-songs", "mouseenter", function() {
+    $("#menu-songs-list").show();
+    $("#menu-songs").addClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-songs", "mouseleave", function() {
+    $("#menu-songs-list").hide();
+    $("#menu-songs").removeClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-songs-list ul li a", "click", function() {
+    $("#menu-songs-list").hide();
+    $("#menu-songs").removeClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-myfeedback", "mouseenter", function() {
+    $("#menu-myfeedback-list").show();
+    $("#menu-myfeedback").addClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-myfeedback", "mouseleave", function() {
+    $("#menu-myfeedback-list").hide();
+    $("#menu-myfeedback").removeClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-myfeedback-list ul li a", "click", function() {
+    $("#menu-myfeedback-list").hide();
+    $("#menu-myfeedback").removeClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-myaccount", "mouseenter", function() {
+    $("#menu-myaccount-list").show();
+    $("#menu-myaccount").addClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-myaccount", "mouseleave", function() {
+    $("#menu-myaccount-list").hide();
+    $("#menu-myaccount").removeClass("menu-background");
+  });
+
+  $("#menu").delegate("#menu-myaccount-list ul li a", "click", function() {
+    $("#menu-myaccount-list").hide();
+    $("#menu-myaccount").removeClass("menu-background");
+  });
+
+
+
+});
