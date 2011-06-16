e77fe3e7eab55bb132c1118d5ab2fca8e6618b36
Chris Doornink <cdoornink@rhapsody.com>
2011-06-16 09:41:09 -0700

    mostly mysong page stylings

--- app/controllers/myfeedback_controller.rb
+++ app/controllers/myfeedback_controller.rb
@@ -13,6 +13,16 @@ class MyfeedbackController < ApplicationController
   end
 
   def mysongs
+    Song.update(1, {:vocals => 1.3, :songwriting => 2, :musicianship => 0, :creativity => 4, :production => 4.1})
+    Song.update(2, {:vocals => 3.1, :songwriting => 2.5, :musicianship => 0, :creativity => 4, :production => 4.2})
+    Song.update(3, {:vocals => 5.0, :songwriting => 3, :musicianship => 2.1, :creativity => 4, :production => 4.3})
+    Song.update(4, {:vocals => 4.6, :songwriting => 4.6, :musicianship => 4.9, :creativity => 4, :production => 4})
+    Song.update(5, {:vocals => 3.6, :songwriting => 2.2, :musicianship => 3, :creativity => 4, :production => 4})
+    Song.update(6, {:vocals => 4.1, :songwriting => 0.7, :musicianship => 3, :creativity => 4, :production => 4})
+    Song.update(7, {:vocals => 3.4, :songwriting => 0, :musicianship => 3, :creativity => 4, :production => 4})
+    Song.update(8, {:vocals => 4, :songwriting => 2, :musicianship => 3, :creativity => 4, :production => 4})
+    Song.update(9, {:vocals => 1, :songwriting => 2, :musicianship => 3, :creativity => 4, :production => 4})
+
     @songs = Song.find_all_by_user_id(@user_info["id"])
     @vocals = Song.average("vocals")
     @songwriting = Song.average("songwriting")
@@ -20,6 +30,8 @@ class MyfeedbackController < ApplicationController
     @creativity = Song.average("creativity")
     @production = Song.average("production")
     @overall = Song.average("overall")
+
+    @song_ids = Array.new
 
     @song_count = Song.count
 
